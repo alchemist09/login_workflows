5f6579eab66323e1e5d3bee77c5d51e991369118
Luke Olali <mugapedia@gmail.com>
2021-09-18 23:57:20 +0300

    configures the redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,21 +4,45 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { Router, Route, browserHistory } from 'react-router'
+// Redux
+import { applyMiddleware, createStore, compose } from 'redux'
+import { Provider } from 'react-redux'
 
+// Redux-Saga
+import createSagaMiddleware from 'redux-saga'
+
+// React Router
+import { Router, Route } from 'react-router'
+
+// Components
 import Login from './login'
 import Signup from './signup'
 import Widgets from './widgets'
 
+// Root Reducer & Root Saga
+import rootReducer from './index-reducer'
+import rootSaga from './index-sagas'
+
+const sagaMiddleware = createSagaMiddleware()
+
+const compposeSetup = process.env.NODE_ENV !== 'production' && typeof window == 'object' 
+                      && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
+
+const store = createStore(rootReducer, compposeSetup(applyMiddleware(sagaMiddleware)))
+
+sagaMiddleware.run(rootSaga)
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/widgets" component={Widgets} />
-      </Route>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Route path="/" component={App}>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/widgets" component={Widgets} />
+        </Route>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
