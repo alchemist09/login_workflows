d0b42c7874d18101bf5559cef1640f14af377c36
Luke Olali <mugapedia@gmail.com>
2021-09-19 00:13:57 +0300

    abstracts the store creation and configuration logic to its file

---
+++ frontend/src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import createSagaMiddleware from '@redux-saga/core'
+
+import rootReducer from '../index-reducer'
+import rootSaga from '../index-sagas'
+
+const configureStore = () => {
+  const sagaMiddleware = createSagaMiddleware()
+  const compposeSetup = process.env.NODE_ENV !== 'production' && typeof window == 'object' 
+                      && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
+  const store = createStore(rootReducer, compposeSetup(applyMiddleware(sagaMiddleware)))
+
+  sagaMiddleware.run(rootSaga)
+  return store
+}
+
+export default configureStore
