f0ea82a714875a799b4bca3e5a1ea9ced122c052
Luke Olali <mugapedia@gmail.com>
2021-09-22 00:52:09 +0300

    adds the clientReducer to the rootReducer

--- frontend/src/index-reducer.js
+++ frontend/src/index-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { reducer as form } from 'redux-form'
+import clientReducer from './client/reducer'
 
 const rootReducer = combineReducers({
-  form
+  form,
+  client: clientReducer
 })
 
 export default rootReducer
