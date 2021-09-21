aa27769b502e03fea83366a5026ff9dc22d97519
Luke Olali <mugapedia@gmail.com>
2021-09-22 01:33:41 +0300

    adds signup reducer to the root reducer

--- frontend/src/index-reducer.js
+++ frontend/src/index-reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import { reducer as form } from 'redux-form'
 import clientReducer from './client/reducer'
+import signup from './client/reducer'
 
 const rootReducer = combineReducers({
   form,
-  client: clientReducer
+  client: clientReducer,
+  signup
 })
 
 export default rootReducer
