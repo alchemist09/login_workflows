2ea990ef4068bdef43fbc59eeffe0cc3728a9b72
Luke Olali <mugapedia@gmail.com>
2021-09-22 01:13:36 +0300

    defines the signupReducer

--- frontend/src/signup/reducer.js
+++ frontend/src/signup/reducer.js
@@ -1,6 +1,23 @@
+import { SIGN_UP_REQUESTING } from "./constants"
+
 const initialState = {
   requesting: false,
   successful: false,
   messages: [],
   errors: []
 }
+
+const signupReducer = (state=initialState, action) => {
+  switch(action.type) {
+    case SIGN_UP_REQUESTING:
+      return {
+        ...initialState,
+        requesting: true
+      }
+    
+    default:
+      return state
+  }
+}
+
+export default signupReducer
