3b9d7e8a1574acfab46c39c4273ee3ddd7a9cb49
Luke Olali <mugapedia@gmail.com>
2021-09-22 01:39:34 +0300

    fixes typo on the import path of signup reducer

--- frontend/src/index-reducer.js
+++ frontend/src/index-reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 import { reducer as form } from 'redux-form'
 import clientReducer from './client/reducer'
-import signup from './client/reducer'
+import signup from './signup/reducer'
 
 const rootReducer = combineReducers({
   form,
