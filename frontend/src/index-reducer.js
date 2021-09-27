9bcc24dea7021351c3434a702b2be6296ed847f4
Luke Olali <mugapedia@gmail.com>
2021-09-27 23:49:50 +0300

    changes the clientReducer in root reducer to make use of short property names for consistency

--- frontend/src/index-reducer.js
+++ frontend/src/index-reducer.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux'
 import { reducer as form } from 'redux-form'
-import clientReducer from './client/reducer'
+import client from './client/reducer'
 import signup from './signup/reducer'
 
 const rootReducer = combineReducers({
   form,
-  client: clientReducer,
+  client,
   signup
 })
 
