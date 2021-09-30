dff8587b761e366745e62f573217ebcecf696853
Luke Olali <mugapedia@gmail.com>
2021-10-01 01:29:20 +0300

    fixes missing 'type' key in actions dispatched from the signUpUser worker saga

--- frontend/src/signup/sagas.js
+++ frontend/src/signup/sagas.js
@@ -16,9 +16,9 @@ function* signUpUser(action) {
   try {
     const { email, password } = action
     const result = yield call(signUpApi, email, password)
-    yield put({SIGN_UP_SUCCESS, result})
+    yield put({ type: SIGN_UP_SUCCESS, result})
   } catch (error) {
-    yield put({SIGN_UP_ERROR, error})
+    yield put({ type: SIGN_UP_ERROR, error})
   }
 }
 
