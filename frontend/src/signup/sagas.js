f50d560843ce991a005c3f8a73f2bf8572b5aef4
Luke Olali <mugapedia@gmail.com>
2021-10-01 01:45:48 +0300

    refactors the signUpApi method

--- frontend/src/signup/sagas.js
+++ frontend/src/signup/sagas.js
@@ -1,15 +1,18 @@
 import { takeLatest, call, put } from 'redux-saga/effects'
 import { SIGN_UP_REQUESTING, SIGN_UP_SUCCESS, SIGN_UP_ERROR } from './constants'
+import { handleApiErrors } from '../lib/api-errors'
 
 const signup_url = `${process.env.REACT_APP_API_URL}/api/Clients`
 
 function signUpApi(email, password) {
   return fetch(signup_url, {
       method: 'POST',
-      headers: 'Content-Type: application/json',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password })
     }
-  )
+  ).then(handleApiErrors)
+   .then(response => response.json())
+   .catch(error => { throw error })
 }
 
 function* signUpUser(action) {
