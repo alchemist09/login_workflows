9e3b7d7c254bdde660f3902ff78f426c1dac418c
Luke Olali <mugapedia@gmail.com>
2021-10-01 01:05:56 +0300

    defines method that makes API call to sign up user

--- frontend/src/signup/sagas.js
+++ frontend/src/signup/sagas.js
@@ -1,6 +1,17 @@
 import { takeLatest, call, put } from 'redux-saga/effects'
 import { SIGN_UP_REQUESTING, SIGN_UP_SUCCESS, SIGN_UP_ERROR } from './constants'
 
+const signup_url = process.env.REACT_APP_API_URL
+
+function signUpApi(email, password) {
+  return fetch(signup_url, {
+      method: 'POST',
+      headers: 'Content-Type: application/json',
+      body: JSON.stringify({ email, password })
+    }
+  )
+}
+
 function* signUpUser(action) {
   try {
     const { email, password } = action
