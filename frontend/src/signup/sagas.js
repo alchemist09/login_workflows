c7529755911be14e46a39a04add8e477106df865
Luke Olali <mugapedia@gmail.com>
2021-10-01 00:55:35 +0300

    defines the worker saga for sign up process

--- frontend/src/signup/sagas.js
+++ frontend/src/signup/sagas.js
@@ -1,8 +1,14 @@
-import { takeLatest } from 'redux-saga/effects'
-import { SIGN_UP_REQUESTING } from './constants'
+import { takeLatest, call, put } from 'redux-saga/effects'
+import { SIGN_UP_REQUESTING, SIGN_UP_SUCCESS, SIGN_UP_ERROR } from './constants'
 
-function* signUpUser() {
-
+function* signUpUser(action) {
+  try {
+    const { email, password } = action
+    const result = yield call(signUpApi, email, password)
+    yield put({SIGN_UP_SUCCESS, result})
+  } catch (error) {
+    yield put({SIGN_UP_ERROR, error})
+  }
 }
 
 function* signUpWatcher() {
