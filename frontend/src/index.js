b271a75e15f7a222abc3df931ad6b4e6718f09ae
Luke Olali <mugapedia@gmail.com>
2021-10-04 20:38:43 +0300

    moves routing functionality to the App component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,31 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import configureStore from './store';
-import { createBrowserHistory } from 'history'
+// import { createBrowserHistory } from 'history'
 
 // React Router
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 // React Redux
 import { Provider } from 'react-redux'
 
 // Components
-import Login from './login'
-import Signup from './signup'
-import Widgets from './widgets'
+// import Login from './login'
+// import Signup from './signup'
+// import Widgets from './widgets'
 
 const store = configureStore()
-const browserHistory = createBrowserHistory()
+// const browserHistory = createBrowserHistory()
 
 ReactDOM.render(
   <StrictMode>
     <Provider store={store}>
-      <Router history={browserHistory}>
-        <Route exact path="/" component={App} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/widgets" component={Widgets} />
-      </Router>
+      <App />
     </Provider>
   </StrictMode>,
   document.getElementById('root')
