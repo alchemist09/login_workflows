c5eec9b38ee46039bad773a5a9d6112817c5130d
Luke Olali <mugapedia@gmail.com>
2021-10-04 20:40:20 +0300

    creates dummy implementation of the Login and Widgets components to enable rendering

--- frontend/src/login/index.js
+++ frontend/src/login/index.js
@@ -1,7 +1,13 @@
 import { Component } from 'react';
 
 class Login extends Component {
-
+  render() {
+    return(
+      <div>
+        <h2>Login</h2>
+      </div>
+    )
+  }
 }
 
 export default Login
--- frontend/src/widgets/index.js
+++ frontend/src/widgets/index.js
@@ -1,7 +1,13 @@
 import { Component } from 'react';
 
 class Widgets extends Component {
-
+  render() {
+    return(
+      <div>
+        <h2>Widgets</h2>
+      </div>
+    )
+  }
 }
 
 export default Widgets
