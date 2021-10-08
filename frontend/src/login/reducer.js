e1d9791ad445915dd40416519363a9f78185b8ee
Luke Olali <mugapedia@gmail.com>
2021-10-08 23:22:41 +0300

    partial definition of login reducer

--- frontend/src/login/reducer.js
+++ frontend/src/login/reducer.js
@@ -1,6 +1,26 @@
+import { LOGIN_REQUESTING, LOGIN_SUCCESS, LOGIN_ERROR} from './constants'
+
 const initalState = {
   requesting: false,
   successful: false,
   messages: [],
   errors: []
 }
+
+const login_reducer = (state=initalState, action) => {
+  switch(action) {
+    case LOGIN_REQUESTING:
+      break
+    
+    case LOGIN_SUCCESS:
+      break
+
+    case LOGIN_ERROR:
+      break
+
+    default:
+      return state
+  }
+}
+
+export default login_reducer
