3a0cc8f478f7ca84f12ad51d0c9a7194ce297da9
Luke Olali <mugapedia@gmail.com>
2021-10-09 23:35:38 +0300

    creates the login form

--- frontend/src/login/index.js
+++ frontend/src/login/index.js
@@ -1,10 +1,30 @@
 import { Component } from 'react';
+import { Field } from 'redux-form'
 
 class Login extends Component {
   render() {
     return(
-      <div>
-        <h2>Login</h2>
+      <div className="login">
+        <form>
+          <h2>Login</h2>
+          <label htmlFor="email">Email</label>
+          <Field
+            name="email"
+            type="text"
+            id="email"
+            className="email"
+            component="input"
+          />
+          <label htmlFor="password">Password</label>
+          <Field
+            name="password"
+            type="password"
+            id="password"
+            className="password"
+            component="input"
+          />
+          <button action="submit">LOGIN</button>
+        </form>
       </div>
     )
   }
