fa8b630a106d4fab1f0d21af5d2da87b3d98788b
Luke Olali <mugapedia@gmail.com>
2021-10-15 02:39:16 +0300

    defines method that connects to the API and trys to login using a username and password

--- frontend/src/login/sagas.js
+++ frontend/src/login/sagas.js
@@ -4,8 +4,23 @@ import { LOGIN_REQUESTING, LOGIN_ERROR, LOGIN_SUCCESS } from './constants'
 import { CLIENT_UNSET } from '../client/constants'
 import { setClient } from '../client/actions'
 import browserHistory from '../history'
+import { handleApiErrors } from '../lib/api-errors'
+
+const loginUrl = `${process.env.REACT_APP_API_URL}/api/Clients/login`
 
 function* logout() {}
+
+const loginApi = (email, password) => {
+  return fetch(loginUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, password })
+  })
+  .then(handleApiErrors)
+  .then(res => res.json())
+  .then(json => json)
+  .catch(error => { throw error })
+}
 
 function* loginFlow(email, password) {
   let token
