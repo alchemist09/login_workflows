630c30b3f7dcb4cdd15527cd1124bf6a15caaa7a
Luke Olali <mugapedia@gmail.com>
2021-10-15 01:53:44 +0300

    defines worker saga for the login process

--- frontend/src/login/sagas.js
+++ frontend/src/login/sagas.js
@@ -1,11 +1,18 @@
-import { take, fork, cancel, call } from 'redux-saga/effects'
+import { take, fork, cancel, call, put } from 'redux-saga/effects'
 
-import { LOGIN_REQUESTING, LOGIN_ERROR } from './constants'
+import { LOGIN_REQUESTING, LOGIN_ERROR, LOGIN_SUCCESS } from './constants'
 import { CLIENT_UNSET } from '../client/constants'
 
 function* logout() {}
 
-function* loginFlow(email, password) {}
+function* loginFlow(email, password) {
+  try {
+    const result = yield call(loginApi, email, password)
+    yield put({ type: LOGIN_SUCCESS, result })
+  } catch (error) {
+    yield put({ type: LOGIN_ERROR, error })
+  }
+}
 
 function* loginWatcher() {
   while(true) {
