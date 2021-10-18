90a98f5675bfe92bdcbf322e9c7d92740daab697
Luke Olali <mugapedia@gmail.com>
2021-10-18 23:51:11 +0300

    checks if one is authorized to access a particular route

--- frontend/src/lib/check-auth.js
+++ frontend/src/lib/check-auth.js
@@ -1,3 +1,5 @@
+import { setClient } from '../client/actions'
+
 export const checkIndexAuthorization = ({ dispatch }) => {
   return (nextState, replace, next) => {
     if(checkAuthorization(dispatch)) {
@@ -8,4 +10,21 @@ export const checkIndexAuthorization = ({ dispatch }) => {
     replace('login')
     next()
   }
+}
+
+const checkAuthorization = dispatch => {
+  const storedToken = localStorage.getItem('token')
+  if(storedToken) {
+    const token = JSON.parse(storedToken)
+    const createdDate = new Date(token.createdDate)
+    const created = Math.round(createdDate.getTime() / 1000)
+    const ttl = 1209600
+    const expiry = created + ttl
+
+    if(created > expiry) return false
+
+    dispatch(setClient(token))
+    return true
+  }
+  return false
 }
