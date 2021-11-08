fcc9720ee9cf2cb723fba49ba298df9ba88f37e2
Luke Olali <mugapedia@gmail.com>
2021-11-08 13:08:07 +0300

    defines layout component

---
+++ frontend/src/layout/layout.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import logo from '../logo.svg';
+import '../App.css';
+
+const Layout = (props) => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to Widget Factory</h2>
+      </header>
+      <section className="App-body">
+        <p>This is the general theme of the site</p>
+        {props.children}
+      </section>
+    </div>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.elementType
+}
+
+
+export default Layout
