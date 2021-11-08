250bac2e64cd6ca9d4cd3d46d9bda1b306b71017
Luke Olali <mugapedia@gmail.com>
2021-11-08 13:09:43 +0300

    creates routing for the application

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,43 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import browserHistory from './history'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import configureStore from './store';
 import { Provider } from 'react-redux'
 
+import Layout from './layout/layout';
+import App from './App';
+import Login from './login'
+import Signup from './signup'
+import Widgets from './widgets'
+
+const logged_in = false
 const store = configureStore()
 
 ReactDOM.render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter history={browserHistory}>
+        <Switch>
+          <Route exact path="/">
+            <App />
+          </Route>
+          <Route path="/signup">
+            <Layout><Signup /></Layout>
+          </Route>
+          <Route path="/login">
+            <Layout><Login /></Layout>
+          </Route>
+          <Route>
+            { logged_in ? <Layout><Widgets /></Layout> : <Redirect to={{
+              pathname: "/login"
+            }} />}
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
   document.getElementById('root')
