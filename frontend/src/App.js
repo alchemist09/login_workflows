3e39bf76a7b89f6c7344514661837440ab2e2754
Luke Olali <mugapedia@gmail.com>
2021-11-08 13:07:34 +0300

    removes router from App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import browserHistory from './history'
-import { Router, Route, Switch } from 'react-router-dom'
-import Login from './login'
-import Signup from './signup'
-import Widgets from './widgets'
 
 function App() {
   return (
@@ -14,22 +9,7 @@ function App() {
         <h2>Welcome to Widget Factory</h2>
       </header>
       <section className="App-body">
-        <Router history={browserHistory}>
-          <Switch>
-            <Route exact path="/">
-              <Signup />
-            </Route>
-            <Route path="/signup">
-              <Signup />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/widgets">
-              <Widgets />
-            </Route>
-          </Switch>
-        </Router>
+      
       </section>
     </div>
   );
