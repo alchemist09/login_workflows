fc0636103fad5cab8a9a50527a08b9dcc6dfbb81
Luke Olali <mugapedia@gmail.com>
2021-11-16 23:39:30 +0300

    defines the widgets reducer

--- frontend/src/widgets/reducer.js
+++ frontend/src/widgets/reducer.js
@@ -1,3 +1,5 @@
+import { WIDGET_CREATING, WIDGET_CREATE_SUCCESS, WIDGET_CREATE_ERROR} from "./constants"
+
 const initialState = {
   list: [],
   requesting: false,
@@ -5,3 +7,51 @@ const initialState = {
   messages: [],
   errors: []
 }
+
+const widgetReducer = (state=initialState, action) => {
+  switch(action.type) {
+    case WIDGET_CREATING:
+      return {
+        ...state,
+        requesting: true,
+        messages: [
+          {
+            body: `Widget ${action.widget.name} being created.....`,
+            time: new Date()
+          }
+        ]
+      }
+    
+    case WIDGET_CREATE_SUCCESS:
+      return {
+        ...state,
+        requesting: false,
+        successful: true,
+        messages: [
+          {
+            body: `Widget ${action.widget.name} has been created`,
+            time: new Date()
+          }
+        ]
+      }
+
+    case WIDGET_CREATE_ERROR:
+      return {
+        ...state,
+        requesting: false,
+        successful: false,
+        messages: [],
+        errors: state.errors.concat([
+          {
+            body: action.error.toString(),
+            time: new Date()
+          }
+        ])
+      }
+
+    default:
+      return state
+  }
+}
+
+export default widgetReducer
