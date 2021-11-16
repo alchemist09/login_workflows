45747b65b43353290dd841ed828a09385b78ffad
Luke Olali <mugapedia@gmail.com>
2021-11-16 23:54:46 +0300

    adds the widgetReducer to the root reducer

--- frontend/src/index-reducer.js
+++ frontend/src/index-reducer.js
@@ -4,13 +4,15 @@ import { connectRouter } from 'connected-react-router'
 import client from './client/reducer'
 import signup from './signup/reducer'
 import login from './login/reducer'
+import wisgets from './widgets/reducer'
 
 const rootReducer = history => combineReducers({
   router: connectRouter(history),
   form,
   client,
   signup,
-  login
+  login,
+  wisgets
 })
 
 export default rootReducer
