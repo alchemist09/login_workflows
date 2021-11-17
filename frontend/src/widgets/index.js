a578997dba740da5713f839e0d18b5bdd38c6ad2
Luke Olali <mugapedia@gmail.com>
2021-11-17 23:59:51 +0300

    creates the widget creation form

--- frontend/src/widgets/index.js
+++ frontend/src/widgets/index.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import { connect } from 'react-redux'
-import { reduxForm } from 'redux-form'
+import { Field, reduxForm } from 'redux-form'
 import { widgetCreate } from './actions'
 
 
@@ -9,8 +9,38 @@ const nameRequired = value => (value ? undefined : 'Name required')
 class Widgets extends Component {
   render() {
     return(
-      <div>
-        <h2>Widgets</h2>
+      <div className="widgets">
+        <h2>CREATE THE WIDGET</h2>
+        <div className="widgets-form">
+          <form>
+            <label htmlFor="name">Name</label>
+            <Field
+              name="name"
+              type="text"
+              id="name"
+              className="name"
+              component={this.renderNameInput}
+              validate={nameRequired}
+            />
+            <label htmlFor="description">Description</label>
+            <Field
+              name="description"
+              type="text"
+              id="description"
+              className="description"
+              component="input"
+            />
+            <label htmlFor="size">Size</label>
+            <Field
+              name="size"
+              type="number"
+              id="size"
+              className="number"
+              component="input"
+            />
+            <button disabled={invalid} action="submit">CREATE</button>
+          </form>
+        </div>
       </div>
     )
   }
