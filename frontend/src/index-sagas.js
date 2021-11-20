88b8adf60e2b4de6f5d49dc577a85e3eb5199437
Luke Olali <mugapedia@gmail.com>
2021-11-21 00:33:03 +0300

    adds the widget creation saga to the root saga

--- frontend/src/index-sagas.js
+++ frontend/src/index-sagas.js
@@ -1,11 +1,13 @@
 import SignupSaga from './signup/sagas'
 import LoginSaga from './login/sagas'
+import WidgetSaga from './widgets/sagas'
 import { all, fork } from '@redux-saga/core/effects'
 
 export default function* rootSaga () {  
   console.log("******* 555555 *********")
   yield all([
     fork(SignupSaga),
-    fork(LoginSaga)
+    fork(LoginSaga),
+    fork(WidgetSaga)
   ])
 }
