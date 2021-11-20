433bce67b7c894abc2101035bc1600e229b285a9
Luke Olali <mugapedia@gmail.com>
2021-11-20 23:59:10 +0300

    defines worker saga that does widget creation work

--- frontend/src/widgets/sagas.js
+++ frontend/src/widgets/sagas.js
@@ -1,5 +1,17 @@
-import { takeLatest } from 'redux-saga/effects'
+import { takeLatest, call, put } from 'redux-saga/effects'
+import { widgetCreateError, widgetCreateSuccess } from './actions'
 import { WIDGET_CREATING} from './constants'
+
+
+function* widgetCreateFlow(action) {
+  try {
+    const { client, widget } = action
+    const createdWidget = yield call(widgetCreateApi, client, widget)
+    yield put(widgetCreateSuccess(createdWidget))
+  } catch (error) {
+    yield put(widgetCreateError(error))
+  }
+}
 
 function* widgetWatcher() {
   yield takeLatest(WIDGET_CREATING, widgetCreateFlow)
