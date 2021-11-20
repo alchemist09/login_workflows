0ecbfd7231d99921a427e909c6270beea4d8c71c
Luke Olali <mugapedia@gmail.com>
2021-11-21 00:31:15 +0300

    defines the API method called to create a widget

--- frontend/src/widgets/sagas.js
+++ frontend/src/widgets/sagas.js
@@ -1,7 +1,24 @@
 import { takeLatest, call, put } from 'redux-saga/effects'
+import { handleApiErrors } from '../lib/api-errors'
 import { widgetCreateError, widgetCreateSuccess } from './actions'
 import { WIDGET_CREATING} from './constants'
 
+const widget_url = `${process.env.REACT_APP_API_URL}/api/Clients`
+const widgetCreateApi = (client, widget) => {
+  const url = `${widget_url}/${client.id}/widgets`
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: client.token.id || undefined
+    },
+    body: JSON.stringify(widget)
+  })
+  .then(handleApiErrors)
+  .then(res => res.json())
+  .then(json => json)
+  .catch(error => {throw error})
+}
 
 function* widgetCreateFlow(action) {
   try {
