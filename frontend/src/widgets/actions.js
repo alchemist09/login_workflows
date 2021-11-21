b03d846f24fe2efb6aa6ea38b723c2398d14edc3
Luke Olali <mugapedia@gmail.com>
2021-11-21 22:49:13 +0300

    defines widget requesting action creators

--- frontend/src/widgets/actions.js
+++ frontend/src/widgets/actions.js
@@ -1,4 +1,11 @@
-import { WIDGET_CREATING, WIDGET_CREATE_SUCCESS, WIDGET_CREATE_ERROR } from "./constants"
+import { 
+  WIDGET_CREATING,
+  WIDGET_CREATE_SUCCESS,
+  WIDGET_CREATE_ERROR,
+  WIDGET_REQUESTING,
+  WIDGET_REQUESTING_SUCCESS,
+  WIDGET_REQUESTING_ERROR
+} from "./constants"
 
 export const widgetCreate = (client, widget) => ({
   type: WIDGET_CREATING,
@@ -13,6 +20,21 @@ export const widgetCreateSuccess = widget => ({
 
 export const widgetCreateError = error => ({
   type: WIDGET_CREATE_ERROR,
+  error
+})
+
+export const widgetRequest = client => ({
+  type: WIDGET_REQUESTING,
+  client
+})
+
+export const widgetRequestSuccessful = widget => ({
+  type: WIDGET_REQUESTING_SUCCESS,
+  widget
+})
+
+export const widgetRequestError = error => ({
+  type: WIDGET_REQUESTING_ERROR,
   error
 })
 
